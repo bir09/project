e9123ad6c72c5b6888e0dc3f47391dc15c814be5
Yourname <jibarrios>
2020-04-22 16:24:03 -0400

    I got to excited and push to soon

--- src/test/java/com/bitrix24/pages/Message.java
+++ src/test/java/com/bitrix24/pages/Message.java
@@ -36,21 +36,6 @@ public class Message {
     @FindBy(xpath ="//div[@class='main-ui-filter-sidebar-item']")
     public List<WebElement> fSideBar;
 
-    @FindBy(xpath = "//div[@class='main-ui-filter-sidebar-item'][1]")
-    public WebElement workbtn;
-
-    @FindBy(xpath = "//div[@class='main-ui-filter-sidebar-item'][2]")
-    public WebElement favBtn;
-
-    @FindBy(xpath = "//div[@class='main-ui-filter-sidebar-item'][3]")
-    public WebElement activityBtn;
-
-    @FindBy(xpath = "//div[@class='main-ui-filter-sidebar-item'][4]")
-    public WebElement annBtn;
-
-    @FindBy(xpath = "//div[@class='main-ui-filter-sidebar-item'][5]")
-    public WebElement workFlowBtn;
-
     @FindBy(xpath ="//div[@class='main-ui-filter-search-square main-ui-filter-search-square-preset main-ui-square']")
     public WebElement verifyFilter;
 
